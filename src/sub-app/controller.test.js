2a5c649b48fef32a6f68c8a9c358df736f78aca0
Jesus E. Franco Martinez <jefrancomix@gmail.com>
2019-01-16 14:02:55 -0600

    Minimal testing example

---
+++ src/sub-app/controller.test.js
@@ -0,0 +1,31 @@
+/* eslint-env jest */
+import { Test } from '@nestjs/testing'
+import { SubAppController } from './controller'
+import { SubAppService } from './service'
+
+describe('SubAppController', () => {
+  let controller
+  let service
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      controllers: [SubAppController],
+      providers: [SubAppService]
+    }).compile()
+
+    controller = module.get(SubAppController)
+    service = module.get(SubAppService)
+  })
+
+  describe('hello', () => {
+    it('should say hello', async () => {
+      const expected = 'Hello from the Nest\n'
+
+      const spy = jest.spyOn(service, 'getHello')
+
+      expect(await controller.getHello()).toBe(expected)
+
+      expect(spy).toBeCalled()
+    })
+  })
+})
